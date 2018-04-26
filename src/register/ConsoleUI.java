b2ae6ebc0bc50418b81575322699e832271e8b77
Peter Dobeš <peter.dobes@fpt.sk>
2018-04-26 10:31:11 +0200

    Initial commit of register

---
+++ src/register/ConsoleUI.java
@@ -0,0 +1,146 @@
+package register;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * User interface of the application.
+ */
+public class ConsoleUI {
+    /** register.Register of persons. */
+    private Register register;
+    
+    /**
+     * In JDK 6 use Console class instead.
+     * @see //readLine()
+     */
+    private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    
+    /**
+     * Menu options.
+     */
+    private enum Option {
+        PRINT, ADD, UPDATE, REMOVE, FIND, EXIT
+    };
+    
+    public ConsoleUI(Register register) {
+        this.register = register;
+    }
+    
+    public void run() {
+        while (true) {
+            switch (showMenu()) {
+                case PRINT:
+                    printRegister();
+                    break;
+                case ADD:
+                    addToRegister();
+                    break;
+                case UPDATE:
+                    updateRegister();
+                    break;
+                case REMOVE:
+                    removeFromRegister();
+                    break;
+                case FIND:
+                    findInRegister();
+                    break;
+                case EXIT:
+                    return;
+            }
+        }
+    }
+    
+    private String readLine() {
+        //In JDK 6.0 and above Console class can be used
+        //return System.console().readLine();
+        
+        try {
+            return input.readLine();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+    
+    private Option showMenu() {
+        System.out.println("Menu.");
+        for (Option option : Option.values()) {
+            System.out.printf("%d. %s%n", option.ordinal() + 1, option);
+        }
+        System.out.println("-----------------------------------------------");
+        
+        int selection = -1;
+        do {
+            System.out.println("Option: ");
+            selection = Integer.parseInt(readLine());
+        } while (selection <= 0 || selection > Option.values().length);
+        
+        return Option.values()[selection - 1];
+    }
+
+    private void printRegister() {
+        for(int i = 0; i < register.getCount(); i++) {
+            System.out.println((i+1)+". "+register.getPerson(i).toString());
+        }
+    }
+    
+    private void addToRegister() {
+        System.out.println("Enter Name: ");
+        String name = readLine();
+        System.out.println("Enter Phone Number: ");
+        String phoneNumber = readLine();
+        for(int i = 0; i < register.getCount(); i++) {
+            if(register.getPerson(i).getName().equals(name) ||
+                    register.getPerson(i).getPhoneNumber().equals(phoneNumber)) {
+                System.out.println("Person with this name or phone number already exists");
+                return;
+            }
+        }
+        register.addPerson(new Person(name, phoneNumber));
+    }
+
+    private void updateRegister() {
+        int index = 0;
+        printRegister();
+        do {
+            System.out.println("Person's number: ");
+            index = Integer.parseInt(readLine());
+        } while (index <= 0 || index > register.getCount());
+
+        System.out.println("Enter new name: ");
+        String name = readLine();
+        register.getPerson(index-1).setName(name);
+        System.out.println("Enter new phone number: ");
+        String phone = readLine();
+        register.getPerson(index-1).setPhoneNumber(phone);
+    }
+
+    private void findInRegister() {
+        System.out.println("Enter attribute to search by: ");
+        String input = readLine();
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(input);
+        Person result;
+        if(m.matches()) {
+            result = register.findPersonByPhoneNumber(input);
+        } else {
+            result = register.findPersonByName(input);
+        }
+        if(result == null){
+            System.out.println("Nothing found");
+        } else {
+            System.out.println(result.toString());
+        }
+    }
+    
+    private void removeFromRegister() {
+        System.out.println("Enter index: ");
+        int index = Integer.parseInt(readLine());
+        Person person = register.getPerson(index - 1);
+        register.removePerson(person);
+    }
+
+}
---
+++ src/register/Register.java
@@ -0,0 +1,100 @@
+package register;
+
+import java.util.ArrayList;
+
+/**
+ * register.Person register.
+ */
+public class Register {
+    /** register.Person array. */
+    private Person[] persons;
+    
+    /** Number of persons in this register. */
+    private int count;
+    
+    /**
+     * Constructor creates an empty register with maximum size specified.
+     * @param size maximum size of the register
+     */
+    public Register(int size) {
+        persons = new Person[size];
+        count = 0;
+    }
+    
+    /**
+     * Returns the number of persons in this register.
+     * @return the number of persons in this register
+     */
+    public int getCount() {
+        return count;
+    }
+    
+    /**
+     * Returns the maximum number of persons in this register.
+     * @return the maximum number of persons in this register.
+     */
+    public int getSize() {
+        return persons.length;
+    }
+    
+    /**
+     * Returns the person at the specified position in this register.
+     * @param index index of the person to return 
+     * @return person the person at the specified position in this register 
+     */
+    public Person getPerson(int index) {
+        return persons[index];
+    }
+
+    /**
+     * Appends the specified person to the end of this register. 
+     * @param person person to append to this register
+     */
+    public void addPerson(Person person) {
+        persons[count] = person;
+        count++;
+    }       
+
+    /**
+     * Returns the person with specified name in this register or <code>null</code>,
+     * if match can not be found.
+     * @param name name of a person to search for
+     * @return person with specified phone number
+     */
+    public Person findPersonByName(String name) {
+        for(int i = 0; i < getCount(); i++) {
+            if(persons[i].getName().equals(name)) {
+                return persons[i];
+            }
+        }
+        return null;
+    }
+    
+
+    /**
+     * Returns the person with specified phone number in this register or <code>null</code>,
+     * if match can not be found.
+     * @param phoneNumber phone number of a person to search for
+     * @return person with specified phone number
+     */
+    public Person findPersonByPhoneNumber(String phoneNumber) {
+        throw new UnsupportedOperationException("Method findPersonByPhoneNumber not yet implemented");
+    }
+
+    /**
+     * Removes the specified person from the register.
+     * @param person person to remove
+     */
+    public void removePerson(Person person) {
+        boolean fix = false;
+        for(int i = 0; i < getCount(); i++) {
+            if(persons[i].equals(person)) {
+                fix = true;
+                count--;
+            }
+            if(fix) {
+                persons[i] = persons[i+1];
+            }
+        }
+    }
+}
